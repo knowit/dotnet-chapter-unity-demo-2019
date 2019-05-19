1a818856db0c16b4123fd7590fc5c218fc63cd00
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-05-19 14:10:49 +0200

    demo1

---
+++ Assets/Demo1/SimpleMovementBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SimpleMovementBehaviour : MonoBehaviour
+{
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find transform component
+        _transform = GetComponent<Transform>();
+
+        // NOTE:
+        // Since every gameObject is forced to have a
+        // a Transform component, this component is also 
+        // accessible from gameObject.transform
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input axis state
+        var dx = Input.GetAxis("Horizontal");
+        var dy = Input.GetAxis("Vertical");
+
+        // Movement speed
+        var speed = 20;
+
+        // Change the position along the input axis by 20 units per second 
+        _transform.position += new Vector3(dx, dy) * speed * Time.deltaTime;
+    }
+}
